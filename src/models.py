ff162621b8ae993c3826729ffadaa35c13707cc9
Jorge Sánchez Aymar <jorge.s.aymar@gmail.com>
2023-04-07 23:55:19 +0000

    last one

--- src/models.py
+++ src/models.py
@@ -12,10 +12,10 @@ class Customers(Base):
     __tablename__ = 'customers'
     # Here we define columns for the table.
     customerID = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    email = Column(String(250), nullable=True)
-    phonenumber = Column(String(100), nullable=True)
-    address = Column(String(250), nullable=True)
+    name = Column(String(50))
+    email = Column(String(50), nullable=True)
+    phonenumber = Column(String(50), nullable=True)
+    address = Column(String(50), nullable=True)
     isActive = Column(Boolean) 
 
 class Favoritos(Base): 
@@ -30,36 +30,36 @@ class Favoritos(Base):
 class Characters(Base): 
     __tablename__ = 'characters'
     characterID = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    birthYear = Column(String(250))
-    height = Column(String(250))
-    mass = Column(String(250))
-    gender = Column(String(250))
-    hairColor = Column(String(250))
-    skinColor = Column(String(250))
-    homeworld = Column(String(250))
+    name = Column(String(50))
+    birthYear = Column(String(50))
+    height = Column(String(50))
+    mass = Column(String(50))
+    gender = Column(String(50))
+    hairColor = Column(String(50))
+    skinColor = Column(String(50))
+    homeworld = Column(String(50))
     categoryID = Column(Integer, ForeignKey('category.categoryID'))
     favoritos = relationship(Favoritos)
 
 class Planets(Base): 
     __tablename__ = 'planets'
     planetID  = Column(Integer, primary_key=True)
-    name = Column(String(250))
-    population = Column(String(250))
-    rotationPeriod = Column(String(250))
-    orbitalPeriod = Column(String(250))
-    diameter = Column(String(250))
-    gravity = Column(String(250))
-    terrainGlasslands = Column(String(250))
-    surfaceWather = Column(String(250))
-    climate = Column(String(250))
+    name = Column(String(50))
+    population = Column(String(50))
+    rotationPeriod = Column(String(50))
+    orbitalPeriod = Column(String(50))
+    diameter = Column(String(50))
+    gravity = Column(String(50))
+    terrainGlasslands = Column(String(50))
+    surfaceWather = Column(String(50))
+    climate = Column(String(50))
     categoryID = Column(Integer, ForeignKey('category.categoryID'))
     favoritos = relationship(Favoritos)
 
 class Category(Base): 
     __tablename__ = 'category'
     categoryID = Column(Integer, primary_key=True)
-    name = Column(String(250))
+    name = Column(String(50))
     planets = relationship(Planets)
     characters = relationship(Characters)
 
